dffe1c9a72a73f32bdb3f0da4184e562aec7ed37
kyubhin <kyubhinhan@gmail.com>
2023-05-20 14:14:15 +0900

    Feat : user 관련 api 생성

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // user 관련 api
 app.get("/users", async (req, res) => {
@@ -22,6 +23,48 @@ app.get("/users/duplicateCheck", async (req, res) => {
     },
   });
   res.json(user ? true : false);
+});
+
+// 새로운 user 생성
+app.post("/users", async (req, res) => {
+  const { nickName } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      nickName: nickName,
+    },
+  });
+
+  res.json(user);
+});
+
+// user nickName 수정
+app.put("/users", async (req, res) => {
+  const { id, nickName } = req.body;
+
+  const updateUser = await prisma.user.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      nickName: nickName,
+    },
+  });
+
+  res.json(updateUser);
+});
+
+// 기존 user 삭제
+app.delete("/users", async (req, res) => {
+  const { id } = req.query;
+
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  res.json(deleteUser);
 });
 
 app.listen(3000, () =>
